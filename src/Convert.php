6b564df60a4823cf09567bba47bce1b0eedae624
MOHAMAD AL SALTY <mohamad.salty.slice@estadao.com>
2022-12-06 05:48:09 -0300

    chore: phpdoc

--- src/Convert.php
+++ src/Convert.php
@@ -2,9 +2,16 @@
 
 namespace Alsalty\Jsonfile;
 
+/**
+ * Class to do some converts
+ */
 class Convert
 {
 
+    /**
+     * @param $data
+     * @return string
+     */
     static function convertToJson($data): string
     {
         if (is_array($data)) {
